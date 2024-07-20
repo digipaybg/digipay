2658569cc007fe8eba2489744eda65b17a0eb501
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2024-07-20 09:05:42 +0300

    Reimplement the blur effect and overall blur design

--- components/Header.vue
+++ components/Header.vue
@@ -1,46 +1,65 @@
-<script setup lang="ts">
-  const localePath = useLocalePath();
+<script lang="ts" setup>
+const localePath = useLocalePath();
+
+// defineProps({
+//   y: {
+//     type: Number,
+//     required: true,
+//   },
+// });
+const {y} = useWindowScroll({behavior: "smooth"});
 </script>
+<!-- :class="{
+      'shadow-custom': y > 700,
+      'bg-[#0c0427] bg-opacity-20 backdrop-blur': y > 250,
+    }" -->
 
 <template>
   <header
-    class="fixed top-0 z-50 -mb-px w-full border-b border-gray-200 bg-background/75 py-3 backdrop-blur dark:border-gray-800 lg:mb-0 lg:border-0"
+      :style="{
+      backdropFilter: y > 250 ? 'blur(15px) saturate(180%)' : 'none',
+      backgroundColor: y > 250 ? 'rgb(12 4 39 / 0.5) ' : 'transparent',
+      boxShadow: y > 700 ? 'inset 0 -1px 0 0px rgba(255, 255, 255, 0.3)' : 'none',
+    }"
+      class="fixed top-0 z-[1000] h-24 w-full py-3 transition-all duration-300"
   >
-    <div class="flex items-center justify-center gap-3">
+    <div class="flex h-full items-center justify-center gap-3">
       <!-- <NuxtImg alt="DigiPay Logo" preload class="" src="/logo.svg" quality="100" /> -->
-      <Logo class="h-[50px] cursor-pointer rounded-full" @click="() => $router.push('/')" />
+      <Logo class="h-[50px] cursor-pointer rounded-full" @click="() => $router.push('/')"/>
 
-      <a href="/" as-child>
-        <Button variant="ghost" class="bg-transparent">
+      <a as-child href="/">
+        <Button class="bg-transparent" variant="ghost">
           {{ $t("about") }}
         </Button>
       </a>
       <a href="#speakers">
-        <Button variant="ghost" class="bg-transparent">
+        <Button class="bg-transparent" variant="ghost">
           {{ $t("speakers") }}
         </Button>
       </a>
-      <a href="#speakers" as-child>
-        <Button variant="ghost" class="bg-transparent">
+      <a as-child href="#speakers">
+        <Button class="bg-transparent" variant="ghost">
           {{ $t("agenda") }}
         </Button>
       </a>
-      <a href="#schedule" as-child>
-        <Button variant="ghost" class="bg-transparent">
+      <a as-child href="#schedule">
+        <Button class="bg-transparent" variant="ghost">
           {{ $t("partners") }}
         </Button>
       </a>
       <NuxtLink :href="localePath('/blog')" as-child>
-        <Button variant="ghost" class="bg-transparent">
+        <Button class="bg-transparent" variant="ghost">
           {{ $t("blog") }}
         </Button>
       </NuxtLink>
-      <a href="#sponsors" as-child>
-        <Button variant="ghost" class="bg-transparent">
+      <a as-child href="#sponsors">
+        <Button class="bg-transparent" variant="ghost">
           {{ $t("contacts") }}
         </Button>
+
+
       </a>
-      <HeaderLanugageSelector />
+      <HeaderLanugageSelector/>
     </div>
 
     <!-- <HeaderDarkModeToogle /> -->
@@ -49,7 +68,11 @@
 </template>
 
 <style lang="scss">
-  div.absolute.inset-0.-z-10 {
-    mask-image: linear-gradient(to bottom, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0));
-  }
+div.absolute.inset-0.-z-10 {
+  mask-image: linear-gradient(to bottom, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0));
+}
+
+.shadow-custom {
+  box-shadow: inset 0 -1px 0 0 rgba(255, 255, 255, 0.5);
+}
 </style>
