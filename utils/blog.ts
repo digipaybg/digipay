96777c49af9f98467e35953a6d15f80760dc41c6
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2025-06-06 11:47:55 +0300

    feat: Enhance fetchPages function to filter out pages without images

--- utils/blog.ts
+++ utils/blog.ts
@@ -44,6 +44,12 @@ export const fetchPages = async (language: string) => {
             equals: language,
           },
         },
+        {
+          property: "image",
+          rich_text: {
+            is_not_empty: true,
+          },
+        },
       ],
     },
     sorts: [
